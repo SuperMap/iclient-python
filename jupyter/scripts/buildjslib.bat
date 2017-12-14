da0fd2c916ac276f0df88ae5577fa4fdc46cf6fc
liuzhilei <liuzhilei@supermap.com>
2017-12-14 19:56:00 +0800

    完善脚本

--- jupyter/scripts/buildjslib.bat
+++ jupyter/scripts/buildjslib.bat
@@ -1,4 +1,5 @@
 set Current_Dir=%~dp0
 set Project_RootDir=%~dp0..\
 cd /d %Project_RootDir%js
+npm install
 npm run ddl
--- jupyter/scripts/buildjslib.sh
+++ jupyter/scripts/buildjslib.sh
@@ -3,4 +3,5 @@ Current_Dir=$(cd "$(dirname "$0")";pwd)
 Project_RootDir=$(cd $Current_Dir/..;pwd)
 Project_jsDir=$(cd $Current_Dir/../js;pwd)
 cd $Project_jsDir
+npm install
 npm run ddl
