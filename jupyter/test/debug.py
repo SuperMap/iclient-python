73f4eb2795e85fe4d91b4e490c607fc16bbfa6a8
liuzhilei <liuzhilei@supermap.com>
2017-12-15 10:30:42 +0800

    jupyter python代码可以通过debug.py启动服务进行调试

---
+++ jupyter/test/debug.py
@@ -0,0 +1,63 @@
+import os
+import sys
+import subprocess
+import py
+from notebook.nbextensions import install_nbextension_python, uninstall_nbextension_python, enable_nbextension_python, \
+    disable_nbextension_python
+from notebook.notebookapp import main as nbmain
+from os.path import dirname, abspath, join as pjoin
+
+
+def uninstall_develop():
+    rootdir = pjoin(dirname(abspath(__file__)), '..')
+    root_cwd = py.path.local(rootdir)
+    develop_cmd = [
+        sys.executable,
+        'setup.py',
+        'develop',
+        '-u'
+    ]
+    with root_cwd.as_cwd():
+        subprocess.check_call(develop_cmd)
+
+
+def uninstall_nbextension():
+    disable_nbextension_python('iclientpy', sys_prefix=True)
+    uninstall_nbextension_python('iclientpy', sys_prefix=True)
+
+
+def clear_dir():
+    static_dir = pjoin(dirname(abspath(__file__)), '..', 'iclientpy', 'static')
+    build_dir = pjoin(dirname(abspath(__file__)), '..', 'build')
+    os.removedirs(static_dir)
+    os.removedirs(build_dir)
+
+
+def install_develop():
+    rootdir = pjoin(dirname(abspath(__file__)), '..')
+    root_cwd = py.path.local(rootdir)
+    develop_cmd = [
+        sys.executable,
+        'setup.py',
+        'develop'
+    ]
+    with root_cwd.as_cwd():
+        subprocess.check_call(develop_cmd)
+
+
+def install_nbextension():
+    install_nbextension_python('iclientpy', overwrite=True, symlink=True, sys_prefix=True)
+    enable_nbextension_python('iclientpy', sys_prefix=True)
+
+
+def run():
+    rootdir = pjoin(dirname(abspath(__file__)), '..')
+    uninstall_nbextension()
+    uninstall_develop()
+    install_develop()
+    install_nbextension()
+    nbmain(notebook_dir=rootdir)
+
+
+if __name__ == '__main__':
+    run()
