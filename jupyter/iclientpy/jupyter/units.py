25982fd76ad9eaabd645000eee0466c19f990f95
liuzhilei <liuzhilei@supermap.com>
2017-12-27 15:49:47 +0800

    蜂巢图的数据可以从csv文件中读取

--- jupyter/iclientpy/jupyter/units.py
+++ jupyter/iclientpy/jupyter/units.py
@@ -1,5 +1,6 @@
 import os
 import geojson
+import pandas as pd
 
 
 def load_geojson_data():
@@ -13,3 +14,15 @@ def get_privince_geojson_data(geojson, name):
         feature = geojson["features"][i]
         if (name in feature["properties"]["name"]):
             return feature
+
+
+def convert_data_frame_to_dataset(data, lng_key=0, lat_key=1, value_key=2):
+    result = []
+    if isinstance(data, pd.DataFrame):
+        for index, row in data.iterrows():
+            point = geojson.Point((data[lng_key][index], data[lat_key][index]))
+            element = {"geometry": point, "count": data[value_key][index]}
+            result.append(element)
+    else:
+        raise Exception("格式不支持")
+    return result
--- jupyter/test/jupyter/test_units.py
+++ jupyter/test/jupyter/test_units.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
-from iclientpy.jupyter import load_geojson_data, get_privince_geojson_data
+import pandas as pd
+from iclientpy.jupyter import load_geojson_data, get_privince_geojson_data, convert_data_frame_to_dataset
 
 
 class TestUnits(TestCase):
@@ -9,3 +10,17 @@ class TestUnits(TestCase):
         feature = get_privince_geojson_data(geojsondata, privName)
         self.assertEqual('天津市', feature["properties"]["name"])
         self.assertEqual([117.215268, 39.120963], feature["properties"]["cp"])
+
+    def test_convert_data_frame_to_dataset(self):
+        data = {"lat": [24.79, 29.77], "lng": [117.65, 115.40], "value": [14.49, 14.06]}
+        df = pd.DataFrame.from_dict(data)
+        data_set = convert_data_frame_to_dataset(df, "lng", "lat", "value")
+        self.assertEqual(len(data_set), 2)
+        self.assertEqual(data_set[0]['geometry']['type'], 'Point')
+        self.assertEqual(data_set[0]['geometry']['coordinates'][0], 117.65)
+        self.assertEqual(data_set[0]['geometry']['coordinates'][1], 24.79)
+        self.assertEqual(data_set[0]['count'], 14.49)
+        self.assertEqual(data_set[1]['geometry']['type'], 'Point')
+        self.assertEqual(data_set[1]['geometry']['coordinates'][0], 115.40)
+        self.assertEqual(data_set[1]['geometry']['coordinates'][1], 29.77)
+        self.assertEqual(data_set[1]['count'], 14.06)
