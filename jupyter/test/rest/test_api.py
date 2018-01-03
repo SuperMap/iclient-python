b5935d105cdff499c473e3195765fbd9a0fb7a08
liuzhilei <liuzhilei@supermap.com>
2018-01-03 09:26:19 +0800

    验证login的post请求的请求体

--- jupyter/test/rest/test_api.py
+++ jupyter/test/rest/test_api.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 from iclientpy.rest.apifactory import APIFactory
 from iclientpy.rest.api.management import *
 import httpretty
+from sure import expect
 from typing import List
 from iclientpy.rest.api.model import Feature
 from iclientpy.dtojson import *
@@ -11,7 +12,7 @@ class TestAPI(TestCase):
     @httpretty.activate
     def test_post_workspace(self):
         httpretty.register_uri(httpretty.POST,
-                               'http://192.168.20.158:8090/iserver/services/security/login.json?username=admin&password=iserver',
+                               'http://192.168.20.158:8090/iserver/services/security/login.json',
                                status=201,
                                set_cookie='JSESSIONID=958322873908FF9CA99B5CB443ADDD5C')
         httpretty.register_uri(httpretty.POST,
@@ -21,6 +22,9 @@ class TestAPI(TestCase):
                                )
 
         facctory = APIFactory('http://192.168.20.158:8090/iserver', 'admin', 'iserver')
+        expect(httpretty.last_request()).to.have.property("body").being.equal(
+            b'{"username": "admin", "password": "iserver"}'
+        )
         mng = facctory.management()
         param = PostWorkspaceParameter()
         param.workspaceConnectionInfo = 'World.sxwu'
@@ -31,7 +35,7 @@ class TestAPI(TestCase):
     @httpretty.activate
     def test_post_feature(self):
         httpretty.register_uri(httpretty.POST,
-                               'http://192.168.20.158:8090/iserver/services/security/login.json?username=admin&password=iserver',
+                               'http://192.168.20.158:8090/iserver/services/security/login.json',
                                status=201,
                                set_cookie='JSESSIONID=958322873908FF9CA99B5CB443ADDD5C')
 
@@ -42,6 +46,9 @@ class TestAPI(TestCase):
                                )
 
         facctory = APIFactory('http://192.168.20.158:8090/iserver', 'admin', 'iserver')
+        expect(httpretty.last_request()).to.have.property("body").being.equal(
+            b'{"username": "admin", "password": "iserver"}'
+        )
         data_service = facctory.data_service('data-World/rest')
         jsonstr = '[{"fieldNames":["SMID","SMSDRIW","SMSDRIN","SMSDRIE","SMSDRIS","SMUSERID","SMAREA","SMPERIMETER","SMGEOMETRYSIZE","SQKM","SQMI","COLOR_MAP","CAPITAL","COUNTRY","POP_1994","CONTINENT"],"fieldValues":["22","-7.433472633361816","62.35749816894531","-6.38972282409668","61.388328552246094","6","0.25430895154659083","5.743731026651685","4500","1474.69","569.38","5","示例首都a","示例国家a","47067.0","亚洲"],"geometry":{"id":22,"parts":[3],"points":[{"x":-40,"y":60},{"x":-45,"y":62},{"x":-40,"y":55},{"x":-40,"y":60}],"style":null,"type":"REGION"}},{"fieldNames":["SMID","SMSDRIW","SMSDRIN","SMSDRIE","SMSDRIS","SMUSERID","SMAREA","SMPERIMETER","SMGEOMETRYSIZE","SQKM","SQMI","COLOR_MAP","CAPITAL","COUNTRY","POP_1994","CONTINENT"],"fieldValues":["23","-7.433472633361816","62.35749816894531","-6.38972282409668","61.388328552246094","6","0.25430895154659083","5.743731026651685","4500","1474.69","569.38","5","示例首都b","示例国家b","47067.0","亚洲"],"geometry":{"id":23,"parts":[3],"points":[{"x":-40,"y":60},{"x":-45,"y":62},{"x":-40,"y":55},{"x":-40,"y":60}],"style":null,"type":"REGION","prjCoordSys":null}}]'
         features = from_json_str(jsonstr, List[Feature])
