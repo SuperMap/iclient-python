b1e1ade74b92324b093565858fac77c063167340
liuzhilei <liuzhilei@supermap.com>
2018-01-03 08:52:32 +0800

    使用httpretty对requests的请求进行mock测试

--- jupyter/iclientpy/rest/apifactory.py
+++ jupyter/iclientpy/rest/apifactory.py
@@ -1,15 +1,18 @@
 from .proxyfactory import RestInvocationHandler
-from .decorator import  REST,HttpMethod
+from .decorator import REST, HttpMethod
 from .api.management import Management
 from .api.restdata import DataService
 from .proxyfactory import create
-from ..dtojson import from_json_str,to_json_str
+from ..dtojson import from_json_str, to_json_str
 import inspect
 import requests
 from requests.auth import AuthBase
+
 default_session_cookie_name = 'JSESSIONID'
+
+
 class CookieAuth(AuthBase):
-    def __init__(self, cookieValue : str, cookieName : str=default_session_cookie_name):
+    def __init__(self, cookieValue: str, cookieName: str = default_session_cookie_name):
         self._value = cookieValue
         self._name = cookieName
 
@@ -19,7 +22,7 @@ class CookieAuth(AuthBase):
 
 
 class RestInvocationHandlerImpl(RestInvocationHandler):
-    def __init__(self, base_url: str, auth : AuthBase = None, proxies = None):
+    def __init__(self, base_url: str, auth: AuthBase = None, proxies=None):
         self._base_url = base_url
         self._auth = auth
         self._proxies = proxies if proxies is not None else {}
@@ -28,7 +31,8 @@ class RestInvocationHandlerImpl(RestInvocationHandler):
         pass
 
     def post(self, rest, uri, args, kwargs):
-        response = requests.post(self._base_url + uri + '.json', data=to_json_str(kwargs[rest.getEntityKW()]), proxies = self._proxies, auth = self._auth) # type:requests.Response
+        response = requests.post(self._base_url + uri + '.json', data=to_json_str(kwargs[rest.getEntityKW()]),
+                                 proxies=self._proxies, auth=self._auth)  # type:requests.Response
         response.raise_for_status()
         return from_json_str(response.text, inspect.getfullargspec(rest.get_original_func()).annotations['return'])
 
@@ -49,7 +53,7 @@ class RestInvocationHandlerImpl(RestInvocationHandler):
             HttpMethod.DELETE: self.delete,
             HttpMethod.HEAD: self.head
         }
-        uri = rest.getUri() # type:str
+        uri = rest.getUri()  # type:str
         argspec = inspect.getfullargspec(rest.get_original_func())
         kwargs = kwargs.copy()
         names = argspec[0]
@@ -58,26 +62,29 @@ class RestInvocationHandlerImpl(RestInvocationHandler):
         uri = uri.format(**kwargs)
         return methods[rest.getMethod()](rest, uri, args, kwargs)
 
-def createAuth(base_url: str, username: str, passwd: str, token: str, proxies = None) -> AuthBase:
+
+def createAuth(base_url: str, username: str, passwd: str, token: str, proxies=None) -> AuthBase:
     if username is not None and passwd is not None:
         # TODO iPortal和online，iServer CAS登录后续考虑
         # TODO session超时处理，定时刷新保证不超时，以及超时检测重新登录
-        response = requests.post(base_url + '/services/security/login.json', json={'username': username, 'password': passwd}, proxies=proxies)
+        response = requests.post(base_url + '/services/security/login.json',
+                                 json={'username': username, 'password': passwd}, proxies=proxies)
         response.raise_for_status()
         value = response.cookies[default_session_cookie_name]
         return CookieAuth(value)
 
+
 class APIFactory:
-    def __init__(self, base_url: str, username: str = None, passwd: str = None, token: str = None, proxies = None):
+    def __init__(self, base_url: str, username: str = None, passwd: str = None, token: str = None, proxies=None):
         self._base_url = base_url if not base_url.endswith('/') else base_url[:-1]
         self._services_url = self._base_url + '/services'
-        self._proxies = proxies if proxies is not  None else {}
-        auth = createAuth(self._base_url, username, passwd, token, proxies = self._proxies)
-        self._handler = RestInvocationHandlerImpl(self._base_url, auth, proxies = self._proxies)
+        self._proxies = proxies if proxies is not None else {}
+        auth = createAuth(self._base_url, username, passwd, token, proxies=self._proxies)
+        self._handler = RestInvocationHandlerImpl(self._base_url, auth, proxies=self._proxies)
 
     def management(self) -> Management:
         return create(Management, self._handler);
 
-    def data_service(self, service_name:str) -> DataService:
+    def data_service(self, service_name: str) -> DataService:
         handler = RestInvocationHandlerImpl(self._services_url + '/' + service_name, proxies=self._proxies)
         return create(DataService, handler)
--- jupyter/test/rest/test_api.py
+++ jupyter/test/rest/test_api.py
@@ -1,78 +1,49 @@
-from unittest import TestCase, skip
-from iclientpy.dtojson import *
-from http.server import HTTPServer, BaseHTTPRequestHandler
-import threading
+from unittest import TestCase
 from iclientpy.rest.apifactory import APIFactory
 from iclientpy.rest.api.management import *
-import time
+import httpretty
 from typing import List
 from iclientpy.rest.api.model import Feature
+from iclientpy.dtojson import *
 
-
-class Handler(BaseHTTPRequestHandler):
-    def do_POST(self):
-        path = self.path # type:str
-        try:
-            if '/login' in path:
-                self.send_response(201)
-                self.send_header('Set-Cookie', 'JSESSIONID=958322873908FF9CA99B5CB443ADDD5C')
-                self.end_headers()
-                self.wfile.write(bytes('{"succeed":true,"referer":"/iserver/manager"}\n', encoding='utf8'))
-            else:
-                self.send_response(201)
-                self.end_headers()
-                self.wfile.write(bytes('[{"serviceType":"RESTMAP","serviceAddress":"http://127.0.0.1:8090/iserver/services/map-World/rest"}]\n', encoding='utf8'))
-        except Exception:
-            print(Exception)
-
-server_running = threading.Condition()
-running = False
-httpd = HTTPServer(('127.0.0.1', 8091), Handler)
-def run():
-    global server_running, running
-    with server_running:
-        running = True
-        server_running.notify_all()
-    httpd.serve_forever()
-
-server_thread = threading.Thread(target=run)
 
 class TestAPI(TestCase):
-    @classmethod
-    def tearDownClass(cls):
-        httpd.server_close()
-
-    @skip("似乎在TeamCity上跑的时候挂起了")
+    @httpretty.activate
     def test_post_workspace(self):
-        global server_running, running
-        server_thread.start();
-        while not running:
-            with server_running:
-                server_running.wait(1)
-        try:
-            time.sleep(5) #MMP的，实在不知道怎么保证另一个线程里启动的http服务器启动了，我艹
-            proxies = {
-                # 'http': 'http://127.0.0.1:8888/'
-            }
-            facctory = APIFactory('http://127.0.0.1:8091/iserver', 'admin', 'iserver', proxies=proxies)
-            mng = facctory.management()
-            param = PostWorkspaceParameter()
-            param.workspaceConnectionInfo = 'D:\packages\supermap_iserver_7.1.2_win64_zip\samples\data\World\World.sxwu'
-            param.servicesTypes = [ServiceType.RESTMAP]
-            time.sleep(2) # 还不知道怎么在Python上写支持并发的web服务器用于测试，所以，两次请求之间间隔2秒
-            result = mng.postWorkspace(param=param)
-            self.assertEqual(len(result), 1)
-        except Exception:
-            print(Exception)
+        httpretty.register_uri(httpretty.POST,
+                               'http://192.168.20.158:8090/iserver/services/security/login.json?username=admin&password=iserver',
+                               status=201,
+                               set_cookie='JSESSIONID=958322873908FF9CA99B5CB443ADDD5C')
+        httpretty.register_uri(httpretty.POST,
+                               'http://192.168.20.158:8090/iserver/manager/workspaces.json',
+                               status=201,
+                               body='[{"serviceType": "RESTMAP", "serviceAddress": "123"}]'
+                               )
 
-    @skip('暂时只能在本地用本地服务跑，启动mock的http服务还存在问题')
+        facctory = APIFactory('http://192.168.20.158:8090/iserver', 'admin', 'iserver')
+        mng = facctory.management()
+        param = PostWorkspaceParameter()
+        param.workspaceConnectionInfo = 'World.sxwu'
+        param.servicesTypes = [ServiceType.RESTMAP]
+        result = mng.postWorkspace(param=param)
+        self.assertEqual(len(result), 1)
+
+    @httpretty.activate
     def test_post_feature(self):
-            proxies = {
-                'http': 'http://127.0.0.1:8888/'
-            }
-            facctory = APIFactory('http://127.0.0.1:8090/iserver', 'admin', 'iserver', proxies=proxies)
-            data_service = facctory.data_service('data-World/rest')
-            jsonstr = '[{"fieldNames":["SMID","SMSDRIW","SMSDRIN","SMSDRIE","SMSDRIS","SMUSERID","SMAREA","SMPERIMETER","SMGEOMETRYSIZE","SQKM","SQMI","COLOR_MAP","CAPITAL","COUNTRY","POP_1994","CONTINENT"],"fieldValues":["22","-7.433472633361816","62.35749816894531","-6.38972282409668","61.388328552246094","6","0.25430895154659083","5.743731026651685","4500","1474.69","569.38","5","示例首都a","示例国家a","47067.0","亚洲"],"geometry":{"id":22,"parts":[3],"points":[{"x":-40,"y":60},{"x":-45,"y":62},{"x":-40,"y":55},{"x":-40,"y":60}],"style":null,"type":"REGION"}},{"fieldNames":["SMID","SMSDRIW","SMSDRIN","SMSDRIE","SMSDRIS","SMUSERID","SMAREA","SMPERIMETER","SMGEOMETRYSIZE","SQKM","SQMI","COLOR_MAP","CAPITAL","COUNTRY","POP_1994","CONTINENT"],"fieldValues":["23","-7.433472633361816","62.35749816894531","-6.38972282409668","61.388328552246094","6","0.25430895154659083","5.743731026651685","4500","1474.69","569.38","5","示例首都b","示例国家b","47067.0","亚洲"],"geometry":{"id":23,"parts":[3],"points":[{"x":-40,"y":60},{"x":-45,"y":62},{"x":-40,"y":55},{"x":-40,"y":60}],"style":null,"type":"REGION","prjCoordSys":null}}]'
-            features = from_json_str(jsonstr, List[Feature])
-            result = data_service.postFeatures('World', 'Countries', features)
-            self.assertTrue(result.succeed)
+        httpretty.register_uri(httpretty.POST,
+                               'http://192.168.20.158:8090/iserver/services/security/login.json?username=admin&password=iserver',
+                               status=201,
+                               set_cookie='JSESSIONID=958322873908FF9CA99B5CB443ADDD5C')
+
+        httpretty.register_uri(httpretty.POST,
+                               'http://192.168.20.158:8090/iserver/services/data-World/rest/data/datasources/World/datasets/Countries/features.json',
+                               status=200,
+                               body='{"succeed": true}'
+                               )
+
+        facctory = APIFactory('http://192.168.20.158:8090/iserver', 'admin', 'iserver')
+        data_service = facctory.data_service('data-World/rest')
+        jsonstr = '[{"fieldNames":["SMID","SMSDRIW","SMSDRIN","SMSDRIE","SMSDRIS","SMUSERID","SMAREA","SMPERIMETER","SMGEOMETRYSIZE","SQKM","SQMI","COLOR_MAP","CAPITAL","COUNTRY","POP_1994","CONTINENT"],"fieldValues":["22","-7.433472633361816","62.35749816894531","-6.38972282409668","61.388328552246094","6","0.25430895154659083","5.743731026651685","4500","1474.69","569.38","5","示例首都a","示例国家a","47067.0","亚洲"],"geometry":{"id":22,"parts":[3],"points":[{"x":-40,"y":60},{"x":-45,"y":62},{"x":-40,"y":55},{"x":-40,"y":60}],"style":null,"type":"REGION"}},{"fieldNames":["SMID","SMSDRIW","SMSDRIN","SMSDRIE","SMSDRIS","SMUSERID","SMAREA","SMPERIMETER","SMGEOMETRYSIZE","SQKM","SQMI","COLOR_MAP","CAPITAL","COUNTRY","POP_1994","CONTINENT"],"fieldValues":["23","-7.433472633361816","62.35749816894531","-6.38972282409668","61.388328552246094","6","0.25430895154659083","5.743731026651685","4500","1474.69","569.38","5","示例首都b","示例国家b","47067.0","亚洲"],"geometry":{"id":23,"parts":[3],"points":[{"x":-40,"y":60},{"x":-45,"y":62},{"x":-40,"y":55},{"x":-40,"y":60}],"style":null,"type":"REGION","prjCoordSys":null}}]'
+        features = from_json_str(jsonstr, List[Feature])
+        result = data_service.postFeatures('World', 'Countries', features)
+        self.assertTrue(result.succeed)
