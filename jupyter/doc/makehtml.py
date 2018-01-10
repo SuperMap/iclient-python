8a1c103179935f9fb24c1781b5328c3718e0666e
guyongquan@supermap.com <guyongquan@supermap.com>
2018-01-10 15:02:03 +0800

    压缩构建的doc为zip

--- jupyter/doc/makehtml.py
+++ jupyter/doc/makehtml.py
@@ -1,10 +1,27 @@
 import sys
 from sphinx import main
 from sphinx.apidoc import main as  apimain
+
+def exit_if_not_zero(rt: int):
+    if not rt == 0:
+        sys.exit(rt)
+
+
 argv = [sys.argv[0], '-f', '-o', './source/api', '../iclientpy']
-api_main_result = apimain(argv)
-if not api_main_result is 0:
-    sys.exit(api_main_result)
+exit_if_not_zero(apimain(argv))
 argv = [sys.argv[0], '-M', 'html', 'source', 'build']
 sys.argv = argv #sphinx的main函数里竟然直接访问了sys.argv，所以，修改一下sys.argv
-sys.exit(main(argv))
+exit_if_not_zero(main(argv))
+
+import os
+import zipfile
+
+def zipdir(path, ziph):
+    # ziph is zipfile handle
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            ziph.write(os.path.join(root, file))
+
+zipf = zipfile.ZipFile('doc.zip', 'w', zipfile.ZIP_DEFLATED)
+zipdir('./build/', zipf)
+zipf.close()
