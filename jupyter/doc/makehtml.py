b8d26f58e836e012fa49321d837b8117e8704004
guyongquan@supermap.com <guyongquan@supermap.com>
2018-01-10 14:11:23 +0800

    添加构建API doc

--- jupyter/doc/makehtml.py
+++ jupyter/doc/makehtml.py
@@ -1,5 +1,10 @@
 import sys
 from sphinx import main
-argv = [sys.argv[0], '-M', 'html', 'source', 'build'] #sphinx的main函数里竟然直接访问了sys.argv，所以，修改一下sys.argv
-sys.argv = argv
+from sphinx.apidoc import main as  apimain
+argv = [sys.argv[0], '-f', '-o', './source/api', '../iclientpy']
+api_main_result = apimain(argv)
+if not api_main_result is 0:
+    sys.exit(api_main_result)
+argv = [sys.argv[0], '-M', 'html', 'source', 'build']
+sys.argv = argv #sphinx的main函数里竟然直接访问了sys.argv，所以，修改一下sys.argv
 sys.exit(main(argv))
