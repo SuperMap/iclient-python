6b19363aa9ac5c6bac7fe4600e715119a6b24c57
guyongquan@supermap.com <guyongquan@supermap.com>
2018-01-13 01:02:28 +0800

    登录阿里云docker registry

--- distribution/docker-doc/build.sh
+++ distribution/docker-doc/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+docker login registry.cn-beijing.aliyuncs.com -u guyongquan@outlook.com -p $(cat /data/teamcity_agent/conf/alidockerregistrypwd)
 curl --output ./doc.tar http://ci.ispeco.com:90/guestAuth/repository/download/IClientPy_IClientPyDoc/.lastSuccessful/doc.tar
 docker rmi -f $(docker images | grep iclientpy-supmap-io | awk '{print $3}')
 docker build -t iclientpy/iclientpy-supmap-io:latest .
