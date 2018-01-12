67e293005134c80cf6be807f70094d82f2156714
guyongquan@supermap.com <guyongquan@supermap.com>
2018-01-13 01:26:57 +0800

    继续修正笔误

--- distribution/docker-doc/build.sh
+++ distribution/docker-doc/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker login registry.cn-beijing.aliyuncs.com -u guyongquan@outlook.com -p $(cat /data/teamcity_agent/conf/alidockerregistrypwd)
 curl --output ./doc.tar http://ci.ispeco.com:90/guestAuth/repository/download/IClientPy_IClientPyDoc/.lastSuccessful/doc.tar
-docker rmi -f $(docker images | grep iclientpy-supmap-io | awk '{print $3}')
-docker build -t iclientpy/iclientpy-supmap-io:latest .
-docker tag iclientpy/iclientpy-supmap-io:latest registry-internal.cn-beijing.aliyuncs.com/iclientpy/iclientpy-supmap-io:latest
-docker push registry.cn-beijing.aliyuncs.com/iclientpy/iclientpy-supmap-io:latest
+docker rmi -f $(docker images | grep iclientpy-supermap-io | awk '{print $3}')
+docker build -t iclientpy/iclientpy-supermap-io:latest .
+docker tag iclientpy/iclientpy-supermap-io:latest registry.cn-beijing.aliyuncs.com/iclientpy/iclientpy-supermap-io:latest
+docker push registry.cn-beijing.aliyuncs.com/iclientpy/iclientpy-supermap-io:latest
