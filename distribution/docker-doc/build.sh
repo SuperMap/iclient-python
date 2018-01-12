ddec38db054d581508f3c96cdc3203ecbe83b0a8
guyongquan@supermap.com <guyongquan@supermap.com>
2018-01-12 17:31:49 +0800

    修改下载命令为curl

--- distribution/docker-doc/build.sh
+++ distribution/docker-doc/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget --output-document=./doc.tar http://ci.ispeco.com:90/guestAuth/repository/download/IClientPy_IClientPyDoc/.lastSuccessful/doc.tar
+curl --output ./doc.tar http://ci.ispeco.com:90/guestAuth/repository/download/IClientPy_IClientPyDoc/.lastSuccessful/doc.tar
 docker rmi -f $(docker images | grep iclientpy-supmap-io | awk '{print $3}')
 docker build -t iclientpy/iclientpy-supmap-io:latest .
 docker tag iclientpy/iclientpy-supmap-io:latest registry-internal.cn-beijing.aliyuncs.com/iclientpy/iclientpy-supmap-io:latest
