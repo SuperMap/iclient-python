d560e1c76791d27721cd91288f1702fef6003da3
liuzhilei <liuzhilei@supermap.com>
2018-01-15 11:09:15 +0800

    继续尝试增加teamcity的构建号

--- jupyter/iclientpy/_version.py
+++ jupyter/iclientpy/_version.py
@@ -5,6 +5,6 @@ version_info = (9, 0, 0)
 _specifier_ = {'alpha': 'a', 'beta': 'b', 'candidate': 'rc', 'final': ''}
 
 __version__ = '%s.%s.%s%s' % (version_info[0], version_info[1], version_info[2],
-                              '.' + os.environ.get('build.number') if os.environ.get('build.number') else '')
+                              '.' + os.environ.get('BUILD_NUMBER') if os.environ.get('BUILD_NUMBER') else '')
 
 EXTENSION_VERSION = '^9.0.0'
