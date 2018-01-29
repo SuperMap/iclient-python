79269c2a49ede7f6eaf840c426eccc28204d33ef
liuzhilei <liuzhilei@supermap.com>
2018-01-29 18:37:18 +0800

    fix ut

--- jupyter/iclientpy/rest/__init__.py
+++
@@ -1,5 +0,0 @@
-from .api import *
-from .cmd import *
-from .apifactory import *
-from .decorator import *
-from .proxyfactory import *
--- jupyter/iclientpy/rest/api/__init__.py
+++
@@ -1,4 +0,0 @@
-from .management import *
-from .model import *
-from .restdata import *
-from .updatetileset import *
