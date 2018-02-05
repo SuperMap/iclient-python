e25c8fd46ab3b68c78d726ba717d3fd409da841d
guyongquan <guyongquan@outlook.com>
2018-02-05 16:32:30 +0800

    下载output目录的文件

--- jupyter/iclientpy/rest/api/cacheutils.py
+++ jupyter/iclientpy/rest/api/cacheutils.py
@@ -1,6 +1,8 @@
 from .management import TileSourceInfo, SMTilesTileSourceInfo
 from .model import SMTilesMapProviderSetting, FastDFSTileProviderSetting, MongoDBTileProviderSetting, OTSTileProviderSetting, UGCV5TileProviderSetting, GeoPackageMapProviderSetting
 from os import path as ospath
+from io import BufferedIOBase
+import requests
 
 def _smtiles_provider_setting_to_tilesourceinfo(setting: SMTilesMapProviderSetting) -> SMTilesTileSourceInfo:
     result = SMTilesTileSourceInfo()
@@ -18,9 +20,19 @@ _provider_setting_to_tile_source_info_functions = {
     UGCV5TileProviderSetting : _provider_setting_to_tilesourceinfo_todo,
     GeoPackageMapProviderSetting : _provider_setting_to_tilesourceinfo_todo
 }
+
+
 def _unknown_provider_setting_type(setting):
     raise Exception('unknown type ' + type(setting))
 
 
 def provider_setting_to_tile_source_info(provider_setting) -> TileSourceInfo:
     return _provider_setting_to_tile_source_info_functions.get(type(provider_setting), _unknown_provider_setting_type)(provider_setting)
+
+
+def download_file_from_output_directory(base_url:str, relative_path:str, output:BufferedIOBase):
+    url = (base_url if base_url.endswith('/') else base_url + '/') + relative_path.replace('\\', '/').replace('//', '/')
+    resp = requests.get(url, stream = True)
+    for chunk in resp.iter_content(1024):
+        if chunk:
+            output.write(chunk)
--- jupyter/test/rest/api/test_cacheutils.py
+++ jupyter/test/rest/api/test_cacheutils.py
@@ -1,6 +1,8 @@
 from unittest import TestCase
-from iclientpy.rest.api.cacheutils import provider_setting_to_tile_source_info
+from iclientpy.rest.api.cacheutils import provider_setting_to_tile_source_info, download_file_from_output_directory
 from iclientpy.rest.api.model import FastDFSTileProviderSetting
+import httpretty
+import io
 
 class ProviderSettingToTileSourceInfoTest(TestCase):
     def test_unknown(self):
@@ -12,3 +14,11 @@ class ProviderSettingToTileSourceInfoTest(TestCase):
     def test_not_implemented(self):
         with self.assertRaises(NotImplementedError):
             provider_setting_to_tile_source_info(FastDFSTileProviderSetting())
+
+    @httpretty.activate
+    def test_download_file(self):
+        with io.BytesIO() as output:
+            content = 'content'
+            httpretty.register_uri(httpretty.GET, 'http://localhost:8090/iserver/./output/b.smtiles', content)
+            download_file_from_output_directory('http://localhost:8090/iserver', r'.\output\b.smtiles', output)
+            self.assertIn(content, str(output.getvalue()))
