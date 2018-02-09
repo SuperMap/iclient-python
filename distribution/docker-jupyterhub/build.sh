2c7f5854ec1b04e3f18a63f578a068e381f92978
liuzhilei <liuzhilei@supermap.com>
2018-02-09 15:38:43 +0800

    fix 脚本错误

--- distribution/docker-jupyterhub/build.sh
+++ distribution/docker-jupyterhub/build.sh
@@ -4,5 +4,5 @@ docker rmi -f $(docker images | grep iclientpy/jupyterhub | awk '{print $3}')
 rm ./iclientpy-*.whl
 cp ../../jupyter/dist/iclientpy-*.whl ./
 docker build -t iclientpy/jupyterhub .
-docker tag iclientpy/iclientpy-supermap-io:latest registry.cn-beijing.aliyuncs.com/iclientpy/jupyterhub:latest
+docker tag iclientpy/jupyterhub:latest registry.cn-beijing.aliyuncs.com/iclientpy/jupyterhub:latest
 docker push registry.cn-beijing.aliyuncs.com/iclientpy/jupyterhub:latest
