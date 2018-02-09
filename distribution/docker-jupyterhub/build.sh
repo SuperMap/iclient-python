b60ea9840f93c502cf752511ccda04e9cfa3d613
liuzhilei <liuzhilei@supermap.com>
2018-02-09 15:12:18 +0800

    可以push到阿里云镜像仓库

--- distribution/docker-jupyterhub/build.sh
+++ distribution/docker-jupyterhub/build.sh
@@ -1,3 +1,8 @@
-#!/bin/bash
+#!/bin/bash -e
+docker login registry.cn-beijing.aliyuncs.com -u guyongquan@outlook.com -p $(cat /data/teamcity_agent/conf/alidockerregistrypwd)
+docker rmi -f $(docker images | grep iclientpy/jupyterhub | awk '{print $3}')
+rm ./iclientpy-*.whl
 cp ../../jupyter/dist/iclientpy-*.whl ./
 docker build -t iclientpy/jupyterhub .
+docker tag iclientpy/iclientpy-supermap-io:latest registry.cn-beijing.aliyuncs.com/iclientpy/jupyterhub:latest
+docker push registry.cn-beijing.aliyuncs.com/iclientpy/jupyterhub:latest
