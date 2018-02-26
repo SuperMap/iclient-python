cf216eaea99dc43caf9cc0d814a8225ef11a0793
liuzhilei <liuzhilei@supermap.com>
2018-02-26 10:55:59 +0800

    jupyterhub登录后可含有notebook示例

--- distribution/docker-jupyterhub/build.sh
+++ distribution/docker-jupyterhub/build.sh
@@ -3,6 +3,8 @@ docker login registry.cn-beijing.aliyuncs.com -u guyongquan@outlook.com -p $(cat
 docker rmi -f $(docker images | grep iclientpy/jupyterhub | awk '{print $3}')
 rm ./iclientpy-*.whl
 cp ../../jupyter/dist/iclientpy-*.whl ./
+rm -rf ./sample
+cp ../../jupyter/sample ./sample -r
 docker build -t iclientpy/jupyterhub .
 docker tag iclientpy/jupyterhub:latest registry.cn-beijing.aliyuncs.com/iclientpy/jupyterhub:latest
 docker push registry.cn-beijing.aliyuncs.com/iclientpy/jupyterhub:latest
--- distribution/docker-jupyterhub/jupyterhubextsrc/iclientpyjupyterhubext/suauthenticator.py
+++ distribution/docker-jupyterhub/jupyterhubextsrc/iclientpyjupyterhubext/suauthenticator.py
@@ -29,6 +29,7 @@ class SuAuthenticator(PAMAuthenticator):
         if not SuAuthenticator.system_user_exists(username):
             encPass = crypt.crypt(password, "22")
             os.system("useradd -p " + encPass + " -d " + "/home/" + username + " -m " + username)
+            os.system("cp -r /iclientpy/sample /home/" + username)
         try:
             pamela.authenticate(username, password, service=self.service)
         except pamela.PAMError as e:
