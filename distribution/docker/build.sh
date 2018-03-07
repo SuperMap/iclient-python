c0db1615efa627821e72c05c0eb7e54b0d724593
liuzhilei <liuzhilei@supermap.com>
2018-03-07 10:12:07 +0800

    iclientpy的jupyter notebook镜像

--- distribution/docker/build.sh
+++ distribution/docker/build.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+docker login registry.cn-beijing.aliyuncs.com -u guyongquan@outlook.com -p $(cat /data/teamcity_agent/conf/alidockerregistrypwd)
+docker rmi -f $(docker images | grep supermap/iclientpy-jupyter-notebook | awk '{print $3}')
+rm -rf ./sample
+cp ../../jupyter/sample ./sample -r
+rm ./iclientpy-*.whl
+cp ../../jupyter/dist/iclientpy-*.whl ./
 docker build -t supermap/iclientpy-jupyter-notebook .
+docker tag supermap/iclientpy-jupyter-notebook:latest registry.cn-beijing.aliyuncs.com/supermap/iclientpy-jupyter-notebook:latest
+docker push registry.cn-beijing.aliyuncs.com/supermap/iclientpy-jupyter-notebook:latest
