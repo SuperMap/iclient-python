e3ca534b3bb2715ecd3831141d2e9c8510625a0a
liuzhilei <liuzhilei@supermap.com>
2018-03-24 09:45:08 +0800

    build镜像前先清理上一次下载下来的doc.tar

--- distribution/docker-doc/build.sh
+++ distribution/docker-doc/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker login registry.cn-beijing.aliyuncs.com -u guyongquan@outlook.com -p $(cat /data/teamcity_agent/conf/alidockerregistrypwd)
+rm -rf doc.tar
 curl --output ./doc.tar http://ci.ispeco.com:90/guestAuth/repository/download/IClientPy_IClientPyDoc/.lastSuccessful/doc.tar
 docker rmi -f $(docker images | grep iclientpy-supermap-io | awk '{print $3}')
 docker build -t iclientpy/iclientpy-supermap-io:latest .
