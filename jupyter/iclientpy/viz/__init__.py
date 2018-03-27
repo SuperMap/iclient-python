f1adf66d5791544dc49a0d27e8637c3d0af08fa6
liuzhilei <liuzhilei@supermap.com>
2018-03-27 11:10:01 +0800

    增加geolines方法dosstring

--- jupyter/iclientpy/viz/__init__.py
+++ jupyter/iclientpy/viz/__init__.py
@@ -100,4 +100,14 @@ def honeycomb(*, data, address_key, value_key, fill_style: str = '', shadow_colo
 
 @init_map_decorator(GeoLines)
 def geolines(*, data, names: List[str], symbol_size: int = 15, symbol: str = 'plane', colors: List[str] = None):
+    """
+    生成迁徙图
+
+    Args:
+        data: 数据
+        names: 图例名称
+        symbol_size: 标记大小
+        symbol: 标记类型，可以为：plane,circle,rect,roundRect,triangle,diamond,pin,arrow
+        colors: 颜色
+    """
     pass
