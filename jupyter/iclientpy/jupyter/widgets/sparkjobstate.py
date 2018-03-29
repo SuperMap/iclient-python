5b562505aef5e5cd5d7908948b725cb6f543e8e0
liuzhilei <liuzhilei@supermap.com>
2018-03-29 16:42:25 +0800

    根据实际请求返回数据，毫秒向时间转换 毫秒向秒转

--- jupyter/iclientpy/jupyter/widgets/sparkjobstate.py
+++ jupyter/iclientpy/jupyter/widgets/sparkjobstate.py
@@ -1,25 +1,25 @@
 from ipywidgets import DOMWidget, VBox, Text
+import datetime as dt
 from iclientpy.rest.api.model import SparkJobState
 
 
 class SparkJobStateWidgets(DOMWidget):
-
     runstate_widget = Text(value='0', description='任务状态：', disabled=True)
     start_time_widget = Text(value='0', description='开始时间：', disabled=True)
-    elspsed_time_widget = Text(value='0', description='经过时间：', disabled=True)
-    publisherelapsed_time_widget = Text(value='0', description='发布耗时：', disabled=True)
+    elspsed_time_widget = Text(value='0', description='持续时间(s)：', disabled=True)
+    publisherelapsed_time_widget = Text(value='0', description='发布耗时(s)：', disabled=True)
     end_time_widget = Text(value='0', description='结束时间：', disabled=True)
 
-    vbox = VBox([runstate_widget, start_time_widget, elspsed_time_widget, publisherelapsed_time_widget, end_time_widget])
+    vbox = VBox(
+        [runstate_widget, start_time_widget, elspsed_time_widget, publisherelapsed_time_widget, end_time_widget])
 
     def update(self, state: SparkJobState):
         self.runstate_widget.value = state.runState.value
-        self.start_time_widget.value = str(state.startTime)
-        self.elspsed_time_widget.value = str(state.elapsedTime)
-        self.publisherelapsed_time_widget.value = str(state.publisherelapsedTime)
-        self.end_time_widget.value = str(state.endTime)
+        self.start_time_widget.value = dt.datetime.fromtimestamp(int(state.startTime / 1000)).strftime(
+            '%Y-%m-%d %H:%M:%S')
+        self.elspsed_time_widget.value = str(int(state.elapsedTime / 1000))
+        self.publisherelapsed_time_widget.value = str(int(state.publisherelapsedTime / 1000))
+        self.end_time_widget.value = dt.datetime.fromtimestamp(int(state.endTime / 1000)).strftime('%Y-%m-%d %H:%M:%S')
 
     def _ipython_display_(self, **kwargs):
         self.vbox._ipython_display_(**kwargs)
-
-
--- jupyter/test/jupyter/widgets/test_sparkjobstate.py
+++ jupyter/test/jupyter/widgets/test_sparkjobstate.py
@@ -9,16 +9,16 @@ class SparkJobStateTest(TestCase):
         widget = SparkJobStateWidgets()
         state = SparkJobState()
         state.runState = SparkRunState.RUNNING
-        state.startTime = 1
-        state.elapsedTime = 10
-        state.publisherelapsedTime = 3
-        state.endTime = 11
+        state.startTime = 1522217321932
+        state.elapsedTime = 142259
+        state.publisherelapsedTime = 7889
+        state.endTime = 1522217473360
         widget.update(state)
         self.assertEqual(widget.runstate_widget.value, 'RUNNING')
-        self.assertEqual(widget.start_time_widget.value, '1')
-        self.assertEqual(widget.elspsed_time_widget.value, '10')
-        self.assertEqual(widget.publisherelapsed_time_widget.value, '3')
-        self.assertEqual(widget.end_time_widget.value, '11')
+        self.assertEqual(widget.start_time_widget.value, '2018-03-28 14:08:41')
+        self.assertEqual(widget.elspsed_time_widget.value, '142')
+        self.assertEqual(widget.publisherelapsed_time_widget.value, '7')
+        self.assertEqual(widget.end_time_widget.value, '2018-03-28 14:11:13')
 
     def test_ipytion_display(self):
         widget = SparkJobStateWidgets()
