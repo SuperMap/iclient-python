008ba5464e29e726fb339425cbdf6c8463583c51
liuzhilei <liuzhilei@supermap.com>
2018-04-09 09:48:27 +0800

    jupyter -> iclientpy

--- distribution/docker-jupyterhub/build.sh
+++ distribution/docker-jupyterhub/build.sh
@@ -2,9 +2,9 @@
 docker login registry.cn-beijing.aliyuncs.com -u guyongquan@outlook.com -p $(cat /data/teamcity_agent/conf/alidockerregistrypwd)
 docker rmi -f $(docker images | grep iclientpy/jupyterhub | awk '{print $3}')
 rm ./iclientpy-*.whl
-cp ../../jupyter/dist/iclientpy-*.whl ./
+cp ../../iclientpy/dist/iclientpy-*.whl ./
 rm -rf ./sample
-cp ../../jupyter/sample ./sample -r
+cp ../../iclientpy/sample ./sample -r
 docker build -t iclientpy/jupyterhub .
 docker tag iclientpy/jupyterhub:latest registry.cn-beijing.aliyuncs.com/iclientpy/jupyterhub:latest
 docker push registry.cn-beijing.aliyuncs.com/iclientpy/jupyterhub:latest
--- distribution/docker/build.sh
+++ distribution/docker/build.sh
@@ -2,9 +2,9 @@
 docker login registry.cn-beijing.aliyuncs.com -u guyongquan@outlook.com -p $(cat /data/teamcity_agent/conf/alidockerregistrypwd)
 docker rmi -f $(docker images | grep iclientpy/iclientpy-jupyter-notebook | awk '{print $3}')
 rm -rf ./sample
-cp ../../jupyter/sample ./sample -r
+cp ../../iclientpy/sample ./sample -r
 rm ./iclientpy-*.whl
-cp ../../jupyter/dist/iclientpy-*.whl ./
+cp ../../iclientpy/dist/iclientpy-*.whl ./
 docker build -t iclientpy/iclientpy-jupyter-notebook .
 docker tag iclientpy/iclientpy-jupyter-notebook:latest registry.cn-beijing.aliyuncs.com/iclientpy/iclientpy-jupyter-notebook:latest
 docker push registry.cn-beijing.aliyuncs.com/iclientpy/iclientpy-jupyter-notebook:latest
