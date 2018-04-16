414eb6398a2f69c0ff81decb7984592945aba037
liuzhilei <liuzhilei@supermap.com>
2018-04-16 16:10:08 +0800

    run conda命令无法使用docker cache，通过docker build --cache-from从镜像读取缓存，节省构建时间

--- distribution/docker-jupyterhub/build.sh
+++ distribution/docker-jupyterhub/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 docker login registry.cn-beijing.aliyuncs.com -u guyongquan@outlook.com -p $(cat /data/teamcity_agent/conf/alidockerregistrypwd)
-docker rmi -f $(docker images | grep iclientpy/jupyterhub | awk '{print $3}')
 rm ./iclientpy-*.whl
 cp ../../iclientpy/dist/iclientpy-*.whl ./
 rm -rf ./sample
 cp ../../iclientpy/sample ./sample -r
-docker build -t iclientpy/jupyterhub .
+docker build --cache-from registry.cn-beijing.aliyuncs.com/iclientpy/jupyterhub -t iclientpy/jupyterhub .
 docker tag iclientpy/jupyterhub:latest registry.cn-beijing.aliyuncs.com/iclientpy/jupyterhub:latest
 docker push registry.cn-beijing.aliyuncs.com/iclientpy/jupyterhub:latest
+docker image ls | grep iclientpy/jupyterhub | grep none | awk '{ print $3}' | xargs docker image rm
--- distribution/docker/build.sh
+++ distribution/docker/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 docker login registry.cn-beijing.aliyuncs.com -u guyongquan@outlook.com -p $(cat /data/teamcity_agent/conf/alidockerregistrypwd)
-docker rmi -f $(docker images | grep iclientpy/iclientpy-jupyter-notebook | awk '{print $3}')
 rm -rf ./sample
 cp ../../iclientpy/sample ./sample -r
 rm ./iclientpy-*.whl
 cp ../../iclientpy/dist/iclientpy-*.whl ./
-docker build -t iclientpy/iclientpy-jupyter-notebook .
+docker build --cache-from registry.cn-beijing.aliyuncs.com/iclientpy/iclientpy-jupyter-notebook -t iclientpy/iclientpy-jupyter-notebook .
 docker tag iclientpy/iclientpy-jupyter-notebook:latest registry.cn-beijing.aliyuncs.com/iclientpy/iclientpy-jupyter-notebook:latest
 docker push registry.cn-beijing.aliyuncs.com/iclientpy/iclientpy-jupyter-notebook:latest
+docker image ls | grep iclientpy/iclientpy-jupyter-notebook | grep none | awk '{ print $3}' | xargs docker image rm
