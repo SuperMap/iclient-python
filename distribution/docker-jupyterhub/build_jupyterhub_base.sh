f7269482cb5653f22c2b91ab158918bf072a8a2f
liuzhilei <liuzhilei@supermap.com>
2018-05-17 14:25:52 +0800

    fix 错误，没有指定正确的Dockerfile

--- distribution/docker-jupyterhub/build_jupyterhub_base.sh
+++ distribution/docker-jupyterhub/build_jupyterhub_base.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 docker login registry.cn-beijing.aliyuncs.com -u guyongquan@outlook.com -p $(cat /data/teamcity_agent/conf/alidockerregistrypwd)
-docker build -t iclientpy/jupyterhubbase .
+docker build -t iclientpy/jupyterhubbase -f Dockerfile_base .
 docker tag iclientpy/jupyterhubbase:latest registry.cn-beijing.aliyuncs.com/iclientpy/jupyterhubbase:latest
 docker push registry.cn-beijing.aliyuncs.com/iclientpy/jupyterhubbase:latest
 docker image ls | grep iclientpy/jupyterhubbase | grep none | awk '{ print $3}' | xargs docker image rm || true
