a876f4ed6ec97999b1395b11528373483c911908
liuzhilei <liuzhilei@supermap.com>
2018-05-17 13:40:54 +0800

    解决jupyterhub镜像问题，同时拆分出jupyterhub中conda安装依赖部分为基础镜像

--- distribution/docker-agent/build.sh
+++ distribution/docker-agent/build.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
+#!/bin/bash -e
 docker build -t supermap/iclientpy-teamcity-agent .
--- distribution/docker-jupyterhub/build.sh
+++ distribution/docker-jupyterhub/build.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/bash -e
 docker login registry.cn-beijing.aliyuncs.com -u guyongquan@outlook.com -p $(cat /data/teamcity_agent/conf/alidockerregistrypwd)
 rm ./iclientpy-*.whl
 cp ../../iclientpy/dist/iclientpy-*.whl ./
 rm -rf ./sample
 cp ../../iclientpy/sample ./sample -r
-docker build --cache-from registry.cn-beijing.aliyuncs.com/iclientpy/jupyterhub -t iclientpy/jupyterhub .
+docker build -t iclientpy/jupyterhub .
 docker tag iclientpy/jupyterhub:latest registry.cn-beijing.aliyuncs.com/iclientpy/jupyterhub:latest
 docker push registry.cn-beijing.aliyuncs.com/iclientpy/jupyterhub:latest
 docker image ls | grep iclientpy/jupyterhub | grep none | awk '{ print $3}' | xargs docker image rm || true
