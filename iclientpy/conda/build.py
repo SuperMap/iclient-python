866678a8b5ad67d56ec40252bd76a6d216c44aa6
guyongquan <guyongquan@outlook.com>
2018-06-01 13:22:29 +0800

    添加用于构建的ipython脚本

---
+++ iclientpy/conda/build.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+import os
+import sys
+conda_build_dir = os.path.abspath(os.path.join(sys.argv[0], os.pardir))
+output_dir = os.path.join(conda_build_dir, 'output')
+get_ipython().system('conda build -c https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/conda-forge/ --output-folder {output_dir} iclientpy')
+import pathlib
+cur_platform_name = [name for name in os.listdir('./output') if name != 'noarch'][0]
+channel_dir = os.path.join(conda_build_dir, 'channel')
+cur_platform_dir = os.path.join(channel_dir, cur_platform_name)
+pathlib.Path(channel_dir).mkdir(parents=True, exist_ok=True)
+import shutil
+shutil.copytree(os.path.join(output_dir, cur_platform_name), cur_platform_dir)
+filename = [name for name in os.listdir(cur_platform_dir) if name.endswith('bz2')][0]
+convert_from = os.path.join(cur_platform_dir, filename)
+get_ipython().system('conda convert -o {channel_dir} -p all {convert_from}')
+get_ipython().system('conda index {channel_dir}')
+noarch_dir = os.path.join(channel_dir, 'noarch')
+get_ipython().system('conda index {noarch_dir}')
+import zipfile
+import os
+with zipfile.ZipFile('./conda-package.zip', 'w', zipfile.ZIP_DEFLATED) as zipf:
+    for root,dirs,files in os.walk('./channel'):
+        for file in files:
+            zipf.write(os.path.join(root, file), os.path.join(root, file))
