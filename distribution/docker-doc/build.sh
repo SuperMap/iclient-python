066676c79ad747372fe25b270039726893b8e271
guyongquan <guyongquan@outlook.com>
2018-06-04 14:27:28 +0800

    使用 artifact dependencies 下载依赖的文件，这样可以在日志中看到用的是哪一次构建的artifact

--- distribution/docker-doc/build.sh
+++ distribution/docker-doc/build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 docker login registry.cn-beijing.aliyuncs.com -u guyongquan@outlook.com -p $(cat /data/teamcity_agent/conf/alidockerregistrypwd)
-rm -rf doc.tar
-rm -rf conda_pkg.tar
-curl --output ./conda_pkg.tar http://ci.ispeco.com:90/guestAuth/repository/download/IClientPy_iClientPyCondaPkg/.lastSuccessful/iclientpy/conda/iclientpy-conda-package.tar
-curl --output ./doc.tar http://ci.ispeco.com:90/guestAuth/repository/download/IClientPy_IClientPyDoc/.lastSuccessful/doc.tar
 docker rmi -f $(docker images | grep iclientpy-supermap-io | awk '{print $3}')
 docker build -t iclientpy/iclientpy-supermap-io:latest .
 docker tag iclientpy/iclientpy-supermap-io:latest registry.cn-beijing.aliyuncs.com/iclientpy/iclientpy-supermap-io:latest
