c35ea88c7948c0453da2379fa28c7a21e3d7b644
guyongquan <guyongquan@outlook.com>
2018-06-05 13:42:20 +0800

    直接安装conda 包，cache有不利影响

--- distribution/docker/build.sh
+++ distribution/docker/build.sh
@@ -2,7 +2,7 @@
 docker login registry.cn-beijing.aliyuncs.com -u guyongquan@outlook.com -p $(cat /data/teamcity_agent/conf/alidockerregistrypwd)
 rm -rf ./sample
 cp ../../iclientpy/sample ./sample -r
-docker build --cache-from registry.cn-beijing.aliyuncs.com/iclientpy/iclientpy-jupyter-notebook -t iclientpy/iclientpy-jupyter-notebook .
+docker build iclientpy/iclientpy-jupyter-notebook .
 docker tag iclientpy/iclientpy-jupyter-notebook:latest registry.cn-beijing.aliyuncs.com/iclientpy/iclientpy-jupyter-notebook:latest
 docker push registry.cn-beijing.aliyuncs.com/iclientpy/iclientpy-jupyter-notebook:latest
 docker image ls | grep iclientpy/iclientpy-jupyter-notebook | grep none | awk '{ print $3}' | xargs docker image rm || true
