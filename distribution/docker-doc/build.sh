669314ab3ddbfa5e6c78e4b81c19f9cacd9d126f
guyongquan <guyongquan@outlook.com>
2018-06-07 22:44:35 +0800

    拆分push

--- distribution/docker-doc/build.sh
+++ distribution/docker-doc/build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-docker login registry.cn-beijing.aliyuncs.com -u guyongquan@outlook.com -p $(cat /data/teamcity_agent/conf/alidockerregistrypwd)
 docker rmi -f $(docker images | grep iclientpy-supermap-io | awk '{print $3}')
 docker build -t iclientpy/iclientpy-supermap-io:latest .
 docker tag iclientpy/iclientpy-supermap-io:latest registry.cn-beijing.aliyuncs.com/iclientpy/iclientpy-supermap-io:latest
-docker push registry.cn-beijing.aliyuncs.com/iclientpy/iclientpy-supermap-io:latest
