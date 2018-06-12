67225c91a7278d3a96d38b63605bf76258a957b5
liuzhilei <liuzhilei@supermap.com>
2018-06-12 11:08:48 +0800

    删除不需要的base镜像

--- distribution/docker-jupyterhub/build_jupyterhub_base.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -e
-docker rmi -f $(docker images | grep iclientpy/jupyterhubbase | awk '{print $3}' | uniq) || true
-docker build -t iclientpy/jupyterhubbase -f Dockerfile_base .
-docker tag iclientpy/jupyterhubbase:latest registry.cn-beijing.aliyuncs.com/iclientpy/jupyterhubbase:latest
